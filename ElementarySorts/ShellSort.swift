ed34ebfb1a3c99b0e4a19448cd4fe4210532e692
~j <jmarcelj@me.com>
2017-03-28 16:35:43 -0400

    interim commit before pull

--- ElementarySorts/ShellSort.swift
+++ ElementarySorts/ShellSort.swift
@@ -7,3 +7,24 @@
 //
 
 import Foundation
+
+/// Implementation of an h-sorter
+struct ShellSort {
+
+  /// h-sorts the array
+  /// @returns  an h-sort array using y = 3x+1 as the initialization function
+  func sort(_ array: [Int]) -> [Int] {
+    var array = array
+
+    // h variable
+    // we iterate from 1 to an h >= N/3, where N=array.count
+    var h = 1
+    while (h < (array.count/3)) {
+      h = 1 + 3 * h
+    }
+
+  
+
+    return array
+  }
+}
